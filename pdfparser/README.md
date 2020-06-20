5e0d832d17c57ec7144de0718549a66289d4882e
Lukas Palys <lukq164@gmail.com>
2020-06-20 13:34:58 +0200

    pdf ocr

---
+++ pdfparser/README.md
@@ -0,0 +1,19 @@
+##PDF OCR
+Before run ocr function install required packages, ImageMagick, ghostscript and Tesseract
+###Python packages
+```
+pip install pytesseract
+```
+```
+pip install wand
+```
+###ImageMagick
+Download and install ImageMagick 
+http://docs.wand-py.org/en/latest/guide/install.html#install-imagemagick-on-windows
+###ghostscript
+Download and install ghostscript
+https://www.ghostscript.com/
+###Tesseract
+Download nad install Tesseract
+https://github.com/tesseract-ocr/tessdoc/blob/master/Downloads.md
+In pdfocr function in task.py set path to tesseract.exe
--- pdfparser/tasks.py
+++ pdfparser/tasks.py
@@ -1,6 +1,38 @@
 from scheduler.celery import app
-
+import pytesseract
+from PIL import Image
+from wand.image import Image as wi
 
 @app.task
 def pdfparser():
     print("Hello queue world!")
+
+'''
+Function return parsed text from pdf file using optical character recognition
+path = path to pdf file
+'''
+@app.task
+def pdfocr(path, lang='eng'):
+
+    if len(path) == 0:
+        print('Path is empty')
+        return
+
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' #path to tesseract.exe
+    pdf = wi(filename=path, resolution=300)
+    pdfImage = pdf.convert('jpeg')
+
+    imageBlobs = []
+
+    for img in pdfImage.sequence:
+        imgPage = wi(image=img)
+        imageBlobs.append(imgPage.make_blob('jpeg'))
+
+    result_text = []
+
+    for imgBlob in imageBlobs:
+        im = Image.open(io.BytesIO(imgBlob))
+        text = pytesseract.image_to_string(im, lang=lang)
+        result_text.append(text)
+
+    return result_text
