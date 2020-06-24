25514a061f9ccadae2897ec3e73c3a02c0c2386f
Mikołaj Małkiński <m.malkinski@sotrender.com>
2020-06-24 21:45:53 +0200

    Added Scrapy crawlers
    Done:
    
    * crawler for dziennikustaw.gov.pl
    
    * crawler for monitorpolski.gov.pl
    
    * crawler which searches through all websited in gov.pl domain

--- crawler/tasks.py
+++ crawler/tasks.py
@@ -1,32 +1,61 @@
-# from scheduler.celery import app
-# from pdfparser.tasks import pdfparser
-
-import requests 
-from pathlib import Path
-import CGRT
-import csv
 import codecs
+import csv
+import os
+from pathlib import Path
 
-# @app.task
-# def scan_website(link):
-#     html = curl(link)
-#     parsedhtml = parse(html)
-#     # TODO get links from html
-#     # add links to crawl QUEUE
-#     for scrapped_link in scrapped_links:
-#         scan_website.delay(scrapped_link)
+import CGRT
+import requests
+from billiard.context import Process
+from scrapy import signals
+from scrapy.crawler import Crawler
+from scrapy.utils.project import get_project_settings
+from twisted.internet import reactor
+
+from crawler.gov.gov.spiders.gov import GovDuSpider, GovCrawler, GovMpSpider
+from scheduler.celery import app
 
 
-# @app.task
-# def crawl():
-#     # TODO list of gov links
-#     for link in gov_links:
-#         if pdf:
-#             pdfparser.delay(pdf)
-#         else:
-#             scan_website.delay(link)
+class CrawlerProcess(Process):
+    """ This class allows to run scrapy Crawlers using multiprocessing from billiard """
 
-#     print("Hello queue world!")
+    def __init__(self, spider):
+        Process.__init__(self)
+        os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'crawler.gov.gov.settings')
+        settings = get_project_settings()
+        self.crawler = Crawler(spider.__class__, settings)
+        self.crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
+        self.spider = spider
+
+    def run(self):
+        self.crawler.crawl(self.spider)
+        reactor.run()
+
+
+@app.task
+def crawl_gov_du():
+    """ Starts crawling process which downloads pdfs from dziennikustaw.gov.pl """
+    spider = GovDuSpider()
+    process = CrawlerProcess(spider)
+    process.start()
+    process.join()
+
+
+@app.task
+def crawl_gov_mp():
+    """ Starts crawling process which downloads pdfs from monitorpolski.gov.pl """
+    spider = GovMpSpider()
+    process = CrawlerProcess(spider)
+    process.start()
+    process.join()
+
+
+@app.task
+def crawl_gov():
+    """ Starts crawling process which downloads pdfs from all websites in domain gov.pl """
+    crawler = GovCrawler()
+    process = CrawlerProcess(crawler)
+    process.start()
+    process.join()
 
 
 #download PDF file from url
@@ -36,24 +65,24 @@ def downloadPdf(url, directory = 'tmp', filename = 'document.pdf', chunkSize = 1
     dirPath = Path(directory)
     #create directory if not exist
     dirPath.mkdir(parents=True, exist_ok=True)
-    
+
     #send the request to specified url
     r = requests.get(url, stream = True)
 
-    #reject if not PDF file    
+    #reject if not PDF file
     if 'application/pdf' not in r.headers.get('content-type'):
          print('File under this URL is not PDF')
          return
-    
+
     fullPath = dirPath / filename
     #write to file
     with open(fullPath, "wb") as pdf:
-        
-        #write in chunks in case of big files 
-        for chunk in r.iter_content(chunk_size = chunkSize): 
-            
-            # writing one chunk at a time to pdf file 
-            if chunk: 
+
+        #write in chunks in case of big files
+        for chunk in r.iter_content(chunk_size = chunkSize):
+
+            # writing one chunk at a time to pdf file
+            if chunk:
                 pdf.write(chunk)
 
 
@@ -76,6 +105,6 @@ def downloadCgrtData(country, dateFrom, dateTo):
         if record is None:
             continue
         records.append(record)
-    
+
     #convert downloaded records to DB INPUT
     return CGRT.convertToDatabaseRecords(records)
