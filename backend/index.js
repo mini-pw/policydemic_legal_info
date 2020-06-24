4205703093571eb6307df9de7313ea4ef5f5acc8
Kacper Drejer <kacper.drejer@gmail.com>
2020-06-24 23:13:37 +0200

    Add backend stub method for posting documents

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,36 @@
 const Koa = require('koa');
-const app = new Koa();
+const KoaRouter = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const multer = require('koa-multer');
+const cors = require('@koa/cors');
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+const app = new Koa();
+const router = new KoaRouter();
+const upload = multer();
+
+router.get('/', (ctx) => {
+  ctx.body = "Hello world!"
+})
+
+router.post('/lad', upload.single('pdf'), (ctx) => {
+  console.log(ctx.request)
+  console.log(ctx.req.body)
+  console.log('ctx.req.file', ctx.req.file);
+
+  ctx.status = 200
 });
+
+router.post('/ssd', upload.single('pdf'), (ctx) => {
+  console.log(ctx.request)
+  console.log(ctx.req.body)
+
+  ctx.status = 200
+});
+
+app
+  .use(cors())
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(8000);
