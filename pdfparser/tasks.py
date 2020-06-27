a731833054ce68d5a1e7e3c9fe5c900b6334628b
BogdanJastrzebski <32574029+BogdanJastrzebski@users.noreply.github.com>
2020-06-27 13:46:04 +0200

    asdf

--- pdfparser/tasks.py
+++ pdfparser/tasks.py
@@ -1,6 +1,90 @@
 from scheduler.celery import app
 
+# --- required for complex criterion --- #
+import flair
+from flair.embeddings import WordEmbeddings
+import torch
+import numpy as np
+import pandas
+from flair.data import Sentence
+# -------------------------------------- #
 
 @app.task
 def pdfparser():
     print("Hello queue world!")
+
+# --------- Complex criterion --------- #
+def cos(u,v):
+    return u @ v / u.norm() / v.norm()
+def complex_crit(text, key_words, without=set(), at_least=1, at_most=1, threshold=0.3):
+    """
+    Do common in a sense of embeddings exist?
+
+    Text is a string.
+    key_words is a set.
+    without is a set.
+    """
+
+    t = Sentence(text)
+    emb.embed(t)
+
+    k = Sentence(" ".join(list(key_words)))
+    emb.embed(k)
+
+    if len(without):
+        w = Sentence(" ".join(list(without)))
+        emb.embed(w)
+
+    for tt in t:
+        for kk in k:
+            if cos(tt.embedding, kk.embedding) > threshold:
+                at_least -= 1
+            if at_least == 0:
+                return True
+            if bool(without):
+                if cos(tt.embedding, kk.embedding) > threshold:
+                    at_most -= 1
+                if at_most == 0:
+                    return False
+    return False
+# ------------------------------------- #
+
+# ---------- Simple criterion --------- #
+def simple_crit(text, key_words, without=set(), at_least=1, at_most=1):
+    """
+    Do common words exist in key_words and text?
+
+    Text is a string.
+    key_words is a set.
+    without is a set.
+    """
+    splitted = set(text.split())
+
+    at_least_words = splitted.intersection(set(key_words))
+
+    if len(at_least_words) > (at_least-1):
+        if bool(without):
+            at_most_words = splitted.intersection(set(without))
+            return len(at_most_words) <= (at_most-1)
+        else:
+            return True
+    else: return False
+# ------------------------------------- #
+
+@app.task
+def parse(path, key_words=set(), without=set(), at_least=1, at_most=1, crit="simple"):
+    pdf_text_list=[]
+    with open(path, 'rb') as pdf_file:
+        read_pdf = PyPDF2.PdfFileReader(pdf_file)
+        number_of_pages = read_pdf.getNumPages()
+        c = collections.Counter(range(number_of_pages))
+        for i in c:
+            page = read_pdf.getPage(i)
+            page_content = page.extractText()
+            pdf_text_list.append(page_content)
+        pdf_file.close()
+    if bool(key_words):
+        return pdf_text_list
+    else:
+        crit = simple_crit if crit == "simple" else complex_crit
+        return [t for t in pdf_text_list if crit(t, key_words, without=without, at_least=at_least, at_most=at_most)]
