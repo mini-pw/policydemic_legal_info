fc216e8bb2c08cc262c33a572717bc7ebb13f3fa
Mikołaj Małkiński <m.malkinski@sotrender.com>
2020-07-16 23:28:52 +0200

    Added Dockerfile for celery scheduler and docker-compose for celery and rabbitmq

--- crawler/tasks.py
+++ crawler/tasks.py
@@ -3,7 +3,6 @@ import csv
 import os
 from pathlib import Path
 
-import CGRT
 import requests
 from billiard.context import Process
 from scrapy import signals
@@ -11,6 +10,7 @@ from scrapy.crawler import Crawler
 from scrapy.utils.project import get_project_settings
 from twisted.internet import reactor
 
+from crawler import CGRT
 from crawler.gov.gov.spiders.gov import GovDuSpider, GovCrawler, GovMpSpider
 from scheduler.celery import app
 
