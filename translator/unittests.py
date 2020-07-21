75c56771cc3a76def4341edbc5ee83840c84d3d4
Stanisław Wasilkowski <smwasilkowski@gmail.com>
2020-07-21 19:27:43 +0200

    Update unit test for translator

--- translator/unittests.py
+++ translator/unittests.py
@@ -4,13 +4,13 @@ import unittest
 
 class TestTasks(unittest.TestCase):
     def testTranslate(self):
-        response = translate("testowy tekst")
+        response = translate("Gatunek bardzo zmienny morfologicznie. Organizm jednokomórkowy, o kształcie niesymetrycznym, silnie spłaszczony grzbietobrzusznie, ale z wyraźnymi kolcami (rogami). Komórki o długości od 40 do 450 μm i szerokości 30-75 μm")
         self.assertEqual(
             response,
             {
                 'translation_type': 'auto',
-                'original_text': 'testowy tekst',
-                'translated_text': 'test text',
+                'original_text': 'Gatunek bardzo zmienny morfologicznie. Organizm jednokomórkowy, o kształcie niesymetrycznym, silnie spłaszczony grzbietobrzusznie, ale z wyraźnymi kolcami (rogami). Komórki o długości od 40 do 450 μm i szerokości 30-75 μm',
+                'translated_text': 'A species that is very morphologically variable. A single-celled organism, with a non-symmetrical shape, strongly flattened dorsal, but with clear collars (horns). Cells with a length of 40 to 450 μm and a width of 30-75 μm',
                 'language': 'Polish'})
 
 
