8701a42b7cbddce6875cf41b289ac87aa96ca977
Mateusz Rymuszka <mattrym@gmail.com>
2020-07-22 14:19:31 +0200

    Add base pipeline for processing PDFs

--- nlpengine/tasks.py
+++ nlpengine/tasks.py
@@ -1,37 +1,89 @@
-from scheduler.celery import app
-from elasticsearch import Elasticsearch
-from configparser import ConfigParser
+import os
 
+from elasticsearch import Elasticsearch
+from celery import chain
+from celery.task import subtask
+from configparser import ConfigParser
+from datetime import datetime
+
+from scheduler.celery import app
+import crawler.tasks as crawler_tasks
+import pdfparser.tasks as pdfparser_tasks
+import translator.tasks as translator_tasks
 
 config = ConfigParser()
 config.read('nlpengine/config.ini')
 
 es_hosts = config['elasticsearch']['hosts']
+pdf_rootdir = config['pdf_database']['rootdir']
 es = Elasticsearch(hosts=es_hosts)
 
 INDEX_NAME = 'documents'
-DOC_TYPE = 'document'
+DOC_TYPE = '_doc'
+
+SCRAP_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 
 @app.task
-def insert_document(data):
-    index_document(data)
+def process_pdf_link(pdf_url):
+    print(f"Received pdf: {pdf_url}")
+
+    pdf_chain = \
+        download_pdf.s() | \
+        parse_pdf.s() | \
+        translate_pdf.s() | \
+        process_document.s()
+
+    pdf_chain(pdf_url)
+
 
 @app.task
-def modify_document(document_id, data):
-    update_document(document_id, data)
+def download_pdf(pdf_url):
+    pdf_filename = os.path.basename(pdf_url)
+    pdf_path = os.path.join(pdf_rootdir, pdf_filename)
+
+    crawler_tasks.downloadPdf.apply(kwargs={
+        "url": pdf_url,
+        "directory": pdf_rootdir,
+        "filename": pdf_filename
+    })
+
+    # print(f"PDF URL: {pdf_url}, PDF path: {pdf_path}")
+    return {
+        "web_page": pdf_url,
+        "pdf_path": pdf_path
+    }
 
 
 @app.task
-def process_pdf_link(http_url):
-    """
-    TODO
-    Process pdf link:
-    1. download pdf
-    1. process pdf
-    :param http_url: link to pdf
-    """
-    print(f"Received pdf: {http_url}")
+def parse_pdf(body):
+    pdf_path = body["pdf_path"]
+    parse_result = pdfparser_tasks.parse(pdf_path)
+
+    body.update({"original_text": parse_result})
+    return body
+
+
+@app.task
+def translate_pdf(body):
+    original_text = body["original_text"]
+    result = translator_tasks.translate(original_text)
+
+    body.update(result)
+    return body
+
+
+@app.task
+def process_document(body):
+    scrap_date = datetime.now().strftime(SCRAP_DATE_FORMAT)
+
+    body.update({
+        "document_type": "legalact",
+        "scrap_date": scrap_date,
+        "text_parsing_type": "parser",
+        "keywords": []
+    })
+    index_document(body)
 
 
 def index_document(body):
@@ -46,6 +98,7 @@ def index_document(body):
         doc_type=DOC_TYPE,
         body=body
     )
+
 
 def update_document(id, body):
     """
